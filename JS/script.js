75823639e401009e5db84db59c17add94f8787d3
Adam <adamlkc656@gmail.com>
2026-08-10 21:00:14 +0800

    Perbaiki JavaScript menu

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,19 @@
 // Toggle class active
 const navbarNav = document.querySelector(".navbar-nav");
+const pisangMenu = document.querySelector("#pisang-menu");
 
-// Ketika menu diklik, tambahkan atau hapus kelas 'active' pada elemen menu.
-document.querySelector("#pisang-menu").onclick = () => {
+// Ketika menu diklik
+pisangMenu.onclick = (e) => {
+  e.preventDefault();
   navbarNav.classList.toggle("active");
 };
 
 // Klik di luar sidebar untuk menghilangkan menu
-const pisangMenu = document.querySelector("#pisang-menu");
 document.addEventListener("click", function (e) {
   if (!pisangMenu.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
 });
+
+// Menampilkan Feather Icons
+feather.replace();
